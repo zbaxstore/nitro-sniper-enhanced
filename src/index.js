83f552eb6460deb6cb578f1930fae25cffd56224
GiorgioBrux <18328525+GiorgioBrux@users.noreply.github.com>
2024-04-04 11:09:54 +0200

    refactor: index
    Co-authored-by: codefactor-io <support@codefactor.io>

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. */
 
 global.regex = new RegExp(
-  /(discord\.gift\/|discord\.com\/gifts\/|discordapp\.com\/gifts\/)[^\s]+/gim
+  /(discord\.gift\/|discord\.com\/gifts\/|discordapp\.com\/gifts\/)[^\s]+/gim,
 );
 const privnote = new RegExp(/(?<=privnote.com\/)[^\s]+/);
 const temp_pm = new RegExp(/(?<=temp.pm\/\?)[^\s]+/);
@@ -58,10 +58,10 @@ logging.splash();
 
 logging.info("{blueBright Welcome!}");
 logging.info(
-  `{blueBright Running version} {blueBright.bold ${version}}{blueBright .}`
+  `{blueBright Running version} {blueBright.bold ${version}}{blueBright .}`,
 );
 logging.info(
-  "{redBright This program is licensed under GPL-3.0-or-later and provided free of charge at https://github.com/GiorgioBrux/nitro-sniper-enhanced.}"
+  "{redBright This program is licensed under GPL-3.0-or-later and provided free of charge at https://github.com/GiorgioBrux/nitro-sniper-enhanced.}",
 );
 
 global.userAgent = (() =>
@@ -80,7 +80,7 @@ global.userAgent = (() =>
       logging.error(
         `{red There was an error fetching the latest chrome user agent: ${err
           .toString()
-          .substring(7, err.length)}. Using default...}`
+          .substring(7, err.length)}. Using default...}`,
       );
       return "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
     }))();
@@ -91,11 +91,11 @@ if (
   webhookping_userid === ""
 ) {
   logging.warning(
-    "{rgb(255,245,107) webhookping_userid is not set correctly or is undefined. Defaulting to none.}"
+    "{rgb(255,245,107) webhookping_userid is not set correctly or is undefined. Defaulting to none.}",
   );
 } else {
   logging.info(
-    `{blueBright The userid [${webhookping_userid}] will be pinged in the nitro_webhook.}`
+    `{blueBright The userid [${webhookping_userid}] will be pinged in the nitro_webhook.}`,
   );
 }
 const nitro_webhookclient = new Webhook(nitro_webhookUrl, "nitro");
@@ -103,34 +103,34 @@ const notes_webhookclient = new Webhook(notes_webhookUrl, "notes");
 
 if (!tokens || tokens.length === 0) {
   logging.fatal(
-    "{red There is no token to login to, please check your configuration.}"
+    "{red There is no token to login to, please check your configuration.}",
   );
   logging.fatal("{red Quitting...}");
   process.exit();
 }
 if (useMain !== "true" && useMain !== "false") {
   logging.warning(
-    "{rgb(255,245,107) useMain is not set correctly or is undefined. Defaulting to false.}"
+    "{rgb(255,245,107) useMain is not set correctly or is undefined. Defaulting to false.}",
   );
 }
 if (legitimacyCheck !== "true" && legitimacyCheck !== "false") {
   logging.warning(
-    "{rgb(255,245,107) legitimacyCheck is not set correctly or is undefined. Defaulting to false.}"
+    "{rgb(255,245,107) legitimacyCheck is not set correctly or is undefined. Defaulting to false.}",
   );
 }
 if (obfuscationCheck !== "true" && obfuscationCheck !== "false") {
   logging.warning(
-    "{rgb(255,245,107) obfuscationCheck is not set correctly or is undefined. Defaulting to false.}"
+    "{rgb(255,245,107) obfuscationCheck is not set correctly or is undefined. Defaulting to false.}",
   );
 }
 if (notesCheck !== "true" && notesCheck !== "false") {
   logging.warning(
-    "{rgb(255,245,107) notesCheck is not set correctly or is undefined. Defaulting to false.}"
+    "{rgb(255,245,107) notesCheck is not set correctly or is undefined. Defaulting to false.}",
   );
 }
 if (permanentCache !== "true" && permanentCache !== "false") {
   logging.warning(
-    "{rgb(255,245,107) permanentCache is not set correctly or is undefined. Defaulting to false.}"
+    "{rgb(255,245,107) permanentCache is not set correctly or is undefined. Defaulting to false.}",
   );
 } else if (permanentCache === "true") {
   if (!fs.existsSync("./usedTokens.json")) {
@@ -143,19 +143,19 @@ if (permanentCache !== "true" && permanentCache !== "false") {
         usedTokens = JSON.parse(file);
       } catch (e) {
         logging.info(
-          `{red Couldn't load usedTokens cache because of error: ${e}.}`
+          `{red Couldn't load usedTokens cache because of error: ${e}.}`,
         );
         permanentCache = "false";
       }
     logging.info(
-      `{blueBright Successfully read ${usedTokens.length} tokens from the usedTokens cache.}`
+      `{blueBright Successfully read ${usedTokens.length} tokens from the usedTokens cache.}`,
     );
   }
 }
 
 if (writeNotes !== "true" && writeNotes !== "false") {
   logging.warning(
-    "{rgb(255,245,107) writeNotes is not set correctly or is undefined. Defaulting to false.}"
+    "{rgb(255,245,107) writeNotes is not set correctly or is undefined. Defaulting to false.}",
   );
 } else if (writeNotes === "true") {
   if (!fs.existsSync("./notes")) fs.mkdirSync("./notes"); // Create notes folder if it doesn't exist
@@ -163,7 +163,7 @@ if (writeNotes !== "true" && writeNotes !== "false") {
 
 if (replit !== "true" && replit !== "false") {
   logging.warning(
-    "{rgb(255,245,107) replit is not set correctly or is undefined. Defaulting to false.}"
+    "{rgb(255,245,107) replit is not set correctly or is undefined. Defaulting to false.}",
   );
 } else if (replit === "true") {
   logging.info("{blueBright Running in replit mode.}");
@@ -186,7 +186,7 @@ const paymentsourceid = (() => {
         authorization: mainToken,
         "user-agent": global.userAgent,
       },
-    }
+    },
   );
 
   const ps = JSON.parse(ressyncq.body.toString());
@@ -197,7 +197,7 @@ const paymentsourceid = (() => {
     process.exit();
   } else if (ps.length === 0) {
     logging.warning(
-      "{rgb(255,245,107) Main token does not have a billing source, some codes will not be sniped.}"
+      "{rgb(255,245,107) Main token does not have a billing source, some codes will not be sniped.}",
     );
     return "null";
   } else if (ps[0]) {
@@ -269,7 +269,7 @@ for (const token of tokens) {
             codes.push(
               String(field.value)
                 .replace(/[\])]$/gm, "")
-                .match(global.regex)
+                .match(global.regex),
             );
           }
         }
@@ -305,7 +305,7 @@ for (const token of tokens) {
               writeNotes,
               msg.content.match(privnote).splice(0, 1).toString(),
               notes_webhookclient,
-              client.user.tag
+              client.user.tag,
             );
           case temp_pm.test(msg.content):
             return notes.temp_pm.getData(
@@ -313,7 +313,7 @@ for (const token of tokens) {
               writeNotes,
               msg.content.match(temp_pm).splice(0, 1).toString(),
               notes_webhookclient,
-              client.user.tag
+              client.user.tag,
             );
           default:
             return [];
@@ -328,7 +328,7 @@ for (const token of tokens) {
       const start = new Date();
       code = code.replace(
         /(discord\.gift\/|discord\.com\/gifts\/|discordapp\.com\/gifts\/)/gim,
-        ""
+        "",
       );
       if (obfuscationCheck === "true") {
         const code_no_symbols = code.replace(/\W/g, "");
@@ -356,7 +356,7 @@ for (const token of tokens) {
           logging.success(
             `{rgb(137,96,142) Sniped [${code}] - Fake Code - ${
               msg.guild ? msg.guild.name : "DM"
-            } from ${msg.author.tag}.}`
+            } from ${msg.author.tag}.}`,
           );
           // eslint-disable-next-line no-continue
           continue;
@@ -367,7 +367,7 @@ for (const token of tokens) {
         logging.success(
           `{rgb(255,228,138) Sniped [${code}] - Already checked - Seen in ${
             msg.guild ? msg.guild.name : "DM"
-          } from ${msg.author.tag}.}`
+          } from ${msg.author.tag}.}`,
         );
         // eslint-disable-next-line no-continue
         continue;
@@ -394,7 +394,7 @@ for (const token of tokens) {
                 res.data.subscription_plan.name
               } - ${msg.guild ? msg.guild.name : "DM"} from ${
                 msg.author.tag
-              } - ${end}.}`
+              } - ${end}.}`,
             );
             nitro_webhookclient.send_webhook_nitro(
               res.data.subscription_plan.name,
@@ -404,11 +404,11 @@ for (const token of tokens) {
               end,
               code,
               msg.url,
-              webhookping_userid
+              webhookping_userid,
             );
           } else {
             logging.error(
-              `{red Tried to redeem code [${code}] but got error: ${res.data.message}.}`
+              `{red Tried to redeem code [${code}] but got error: ${res.data.message}.}`,
             );
           }
         })
@@ -417,7 +417,7 @@ for (const token of tokens) {
           if (err.response) {
             if (err.response.status === 401) {
               logging.error(
-                `{red Tried to redeem code [${code}] but the main token is not valid.}`
+                `{red Tried to redeem code [${code}] but the main token is not valid.}`,
               );
             } else if (
               err.response.data.message ===
@@ -427,20 +427,20 @@ for (const token of tokens) {
               logging.success(
                 `{rgb(255,228,138) Sniped [${code}] - Already redeemed - ${
                   msg.guild ? msg.guild.name : "DM"
-                } from ${msg.author.tag} - ${end}.}`
+                } from ${msg.author.tag} - ${end}.}`,
               );
             } else if (
               err.response.data.message ===
               "You need to verify your e-mail in order to perform this action."
             ) {
               logging.error(
-                `{red Tried to redeem code [${code}] but the main token doesn't have a verified e-mail.}`
+                `{red Tried to redeem code [${code}] but the main token doesn't have a verified e-mail.}`,
               );
             } else if (err.response.data.message === "Unknown Gift Code") {
               logging.success(
                 `{redBright Sniped [${code}] - Invalid - ${
                   msg.guild ? msg.guild.name : "DM"
-                } from ${msg.author.tag} - ${end}.}`
+                } from ${msg.author.tag} - ${end}.}`,
               );
             } else if (
               err.response.data.message ===
@@ -448,19 +448,19 @@ for (const token of tokens) {
               err.response.data.message === "Already purchased"
             ) {
               logging.error(
-                `{red Tried to redeem code [${code}] but the gift type cannot be used with an existing Nitro.}`
+                `{red Tried to redeem code [${code}] but the gift type cannot be used with an existing Nitro.}`,
               );
             } else if (
               err.response.data.message ===
               "Payment source required to redeem gift."
             ) {
               logging.success(
-                `{rgb(28,232,41) Sniped [${code}] - You don't have a valid payment method.}`
+                `{rgb(28,232,41) Sniped [${code}] - You don't have a valid payment method.}`,
               );
             }
           } else {
             logging.error(
-              `{red Tried to redeem code [${code}] but got connection error: ${err}}`
+              `{red Tried to redeem code [${code}] but got connection error: ${err}}`,
             );
           }
         });
@@ -473,11 +473,11 @@ for (const token of tokens) {
   client.on("ready", () => {
     if (token === mainToken) {
       logging.info(
-        `{cyan (INFO)} {blue Main token valid: ${client.user.tag} - Sniping in ${client.guilds.cache.size} servers.}`
+        `{cyan (INFO)} {blue Main token valid: ${client.user.tag} - Sniping in ${client.guilds.cache.size} servers.}`,
       );
     } else {
       logging.info(
-        `{cyan Slave logged in as ${client.user.tag} - Sniping in ${client.guilds.cache.size} servers.}`
+        `{cyan Slave logged in as ${client.user.tag} - Sniping in ${client.guilds.cache.size} servers.}`,
       );
     }
     if (token !== mainToken) client.user.setStatus(tokenStatus);
@@ -488,31 +488,34 @@ for (const token of tokens) {
     logging.error(
       `{red Token [${token.substring(
         0,
-        10
-      )}...] encountered a connection error: ${error}.}`
+        10,
+      )}...] encountered a connection error: ${error}.}`,
     );
     client.on("shardResume", () => {
       logging.info(
         `{blueBright Token [${token.substring(
           0,
-          10
-        )}...] reconnected successfully!}`
+          10,
+        )}...] reconnected successfully!}`,
       );
     });
   });
 
-  setTimeout(() => {
-    client.login(token).catch((err) => {
-      if (token === mainToken) {
-        logging.fatal(`{red Main token not valid: ${err}.}`);
-        logging.fatal("{red Quitting...}");
-        process.exit();
-      } else {
-        logging.error(
-          `{red Skipping slave token "${token.substring(0, 10)}...": ${err}.}`
-        );
-        clearTimeout();
-      }
-    });
-  }, Math.floor(Math.random() * (1100 * tokens.length - 500 + 1)) + 500);
+  setTimeout(
+    () => {
+      client.login(token).catch((err) => {
+        if (token === mainToken) {
+          logging.fatal(`{red Main token not valid: ${err}.}`);
+          logging.fatal("{red Quitting...}");
+          process.exit();
+        } else {
+          logging.error(
+            `{red Skipping slave token "${token.substring(0, 10)}...": ${err}.}`,
+          );
+          clearTimeout();
+        }
+      });
+    },
+    Math.floor(Math.random() * (1100 * tokens.length - 500 + 1)) + 500,
+  );
 }
